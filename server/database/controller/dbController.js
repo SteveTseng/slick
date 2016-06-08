fb35fe38fbe97085cfad24ba50e179452e94b76f
Steve Tseng <stevetseng@live.com>
2016-06-08 14:29:42 -0700

    database preliminary setup

---
+++ server/database/controller/dbController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('./../model/dbModel');
+
+module.exports = function(){
+	db.sync({logging: console.log, force: true})
+	.then(() => console.log('synced'));
+};
