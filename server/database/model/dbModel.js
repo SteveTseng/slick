f2101e1d4d8b5520860a40eb17496642104794cf
Steve Tseng <stevetseng@live.com>
2016-08-26 13:31:40 -0700

    db by heroku

--- server/database/model/dbModel.js
+++ server/database/model/dbModel.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://user:stevetseng@live.com:5432/soslick');
+const sequelize = new Sequelize('postgres://iatupkhoswxboy:JZsHbsHx6fi3Wc_lQ8s0CWGPZB@ec2-54-243-47-213.compute-1.amazonaws.com:5432/d9ki06095ksntk
+');
+
 
 
 sequelize.authenticate()
