39e3bfceb6e6b1e8855848a65bf9356c159290cd
Steve Tseng <stevetseng@live.com>
2016-08-26 13:35:33 -0700

    update

--- server/database/model/dbModel.js
+++ server/database/model/dbModel.js
@@ -1,9 +1,20 @@
 'use strict'
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://iatupkhoswxboy:JZsHbsHx6fi3Wc_lQ8s0CWGPZB@ec2-54-243-47-213.compute-1.amazonaws.com:5432/d9ki06095ksntk
-');
+//const sequelize = new Sequelize('postgres://user:stevetseng@live.com:5432/soslick');
 
+var sequelize = new Sequelize('d9ki06095ksntk', 'iatupkhoswxboy', 'JZsHbsHx6fi3Wc_lQ8s0CWGPZB', {
+  host: 'ec2-54-243-47-213.compute-1.amazonaws.com',
+  dialect: 'heroku pg:psql --app heroku-postgres-c73ac359 HEROKU_POSTGRESQL_IVORY',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+
+});
 
 
 sequelize.authenticate()
