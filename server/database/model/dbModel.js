e9f6a1f5e05f1a75fc6be8eafc0cbf3f65aa2ab9
Steve Tseng <stevetseng@live.com>
2016-08-26 13:21:48 -0700

    update

--- server/database/model/dbModel.js
+++ server/database/model/dbModel.js
@@ -1,19 +1,8 @@
 'use strict'
 
 const Sequelize = require('sequelize');
-//const sequelize = new Sequelize('postgres://user:stevetseng@live.com:5432/soslick');
+const sequelize = new Sequelize('postgres://user:stevetseng@live.com:5432/soslick');
 
-var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
-sequelize = new Sequelize(match[5], match[1], match[2], {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging: false,
-    dialectOptions: {
-        ssl: true
-    }
-});
 
 sequelize.authenticate()
 	.then(()=>{console.log('connected!')})
